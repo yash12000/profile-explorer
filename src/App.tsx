b39ee79ff1bdb9430326f328653a53c7ae77b53d
Yash Janbandhu <yashjanbandhu12@gmail.com>
2025-05-23 15:34:20 +0530

    feat: remove unused useState import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { ProfilesProvider } from "./contexts/ProfilesContext";
 import HomePage from "./pages/HomePage";
